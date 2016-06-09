4ca5217311ca5e0430567b2706ec7fbe79ccb41f
Jon Cockayne <benorn@gmail.com>
2016-06-09 13:30:25 +0100

    Add reverse pairs to the operator dict.

--- bayesian_pdes/operator_compilation/compilation_utils.py
+++ bayesian_pdes/operator_compilation/compilation_utils.py
@@ -8,3 +8,12 @@ def infill_op_dict(ops, ops_bar, op_dict):
     for op in ops_bar:
         op_dict[(op, ())] = op_dict[(op,)]
         op_dict[((), op)] = op_dict[(op,)]
+
+    to_add = {}
+    for ops in op_dict:
+        if type(ops) is not tuple or len(ops) != 2: continue
+
+        opposite_pairing = (ops[1], ops[0])
+        if opposite_pairing in op_dict: continue
+        to_add[opposite_pairing] = op_dict[ops]
+    op_dict.update(to_add)
