486baba8128260c0748adf25280f6ad5fb183379
Jon Cockayne <benorn@gmail.com>
2016-06-09 13:30:13 +0100

    Pull out code for stepping forward so it can be called independently.

--- bayesian_pdes/parabolic.py
+++ bayesian_pdes/parabolic.py
@@ -2,19 +2,19 @@ from autograd import numpy as np
 from util.linalg import schur
 import collocation
 import logging
+
 logger = logging.getLogger(__name__)
 
 
 def augment_with_time(spatial_points, time):
-    return np.column_stack([spatial_points, time*np.ones((spatial_points.shape[0], 1))])
+    return np.column_stack([spatial_points, time * np.ones((spatial_points.shape[0], 1))])
 
 
-def solve_parabolic(op_system, times, obs_function, test_function, ics, fun_args=None):
-    ops = op_system.operators
-    ops_bar = op_system.operators_bar
+def solve_parabolic(op_system, ops, ops_bar, times, obs_function, ics, fun_args=None):
 
-    test_t = test_function(times[0])
     all_obs = [ics]
+    all_ops = [[()]]
+    all_ops_bar = [[()]]
 
     # TODO: this is all assuming that we are observing directly the solution at t_0
     init_ops = [()]
@@ -23,77 +23,61 @@ def solve_parabolic(op_system, times, obs_function, test_function, ics, fun_args
     K_0 = collocation.calc_LLbar(init_ops, init_ops_bar, ics, op_system, fun_args)
     last_inv = np.linalg.inv(K_0)
 
-    right, left = collocation.calc_side_matrices(
-        init_ops,
-        init_ops_bar,
-        ics,
-        test_t,
-        op_system,
-        fun_args=fun_args)
-    kern = op_system[()](test_t, test_t, fun_args)
-    covs = [kern - np.dot(left, np.dot(last_inv, right))]
-    rhs = ics[0][1]
-    means = [np.dot(left, np.dot(last_inv, rhs))]
+    posterior_t = collocation.CollocationPosterior(flatten_list(all_ops),
+                                                   flatten_list(all_ops_bar),
+                                                   op_system,
+                                                   flatten_list(all_obs),
+                                                   last_inv,
+                                                   fun_args=fun_args)
 
+    posteriors = [posterior_t]
     for t in times[1:]:
+
         logger.info('t={}'.format(t))
         obs_t = obs_function(t)
 
-        test_t = test_function(t)
-
-        K_t = collocation.calc_LLbar(ops, ops_bar, obs_t, op_system, fun_args)
-        K_t_inv = np.linalg.inv(K_t)
-
-        K_ttm1 = []
-        rmat = []
-        lmat = []
-        for ix, o in enumerate(all_obs):
-            this_ops, this_ops_bar = (ops, ops_bar) if ix != 0 else (init_ops, init_ops_bar)
-            right, left = collocation.calc_side_matrices(
-                this_ops,
-                this_ops_bar,
-                o,
-                test_t,
-                op_system,
-                fun_args=fun_args)
-            rmat.append(right)
-            lmat.append(left)
-            K_ttm1.append(calc_diff_LLbar(this_ops, ops_bar, o, obs_t, op_system, fun_args=fun_args))
-
-        logger.debug('Concatenating K_ttm1; shapes are [{}]'.format([k.shape for k in K_ttm1]))
-        K_ttm1 = np.concatenate(K_ttm1)
-        logger.debug('About to call schur; shapes are: last_inv: {} K_ttm1: {}, K_t: {}; K_t_inv: {}'.format(
-            last_inv.shape,
-            K_ttm1.shape,
-            K_t.shape,
-            K_t_inv.shape
-        ))
-        new_inv = schur(last_inv, K_ttm1, K_t, K_t_inv)
-
-        right, left = collocation.calc_side_matrices(
-            ops,
-            ops_bar,
-            obs_t,
-            test_t,
-            op_system,
-            fun_args=fun_args)
-        rmat.append(right)
-        lmat.append(left)
-        right = np.concatenate(rmat)
-        left = np.concatenate(lmat, axis=1)
-
         all_obs.append(obs_t)
+        all_ops.append(ops)
+        all_ops_bar.append(ops_bar)
 
-        kern = op_system[()](test_t, test_t, fun_args)
-        rhs = np.concatenate([np.concatenate([v for p,v in o]) for o in all_obs])
-        means.append(np.dot(left, np.dot(new_inv, rhs)))
+        posterior_t = step_forward(all_ops, all_ops_bar, all_obs, op_system, fun_args, posteriors[-1])
+        posteriors.append(posterior_t)
 
-        new_cov = kern - np.dot(left, np.dot(new_inv, right))
-        covs.append(new_cov)
+    return posteriors
 
-        last_inv = new_inv
+def step_forward(all_ops, all_ops_bar, all_obs, op_system, fun_args, last_posterior):
+    ops, ops_bar, obs_t = all_ops[-1], all_ops_bar[-1], all_obs[-1]
+    prev_inv = last_posterior.__LLbar_inv__
 
-    return means, covs
+    K_t = collocation.calc_LLbar(ops, ops_bar, obs_t, op_system, fun_args)
+    K_t_inv = np.linalg.inv(K_t)
+
+    K_ttm1 = []
+    for obs, op in zip(all_obs[:-1], all_ops[:-1]):
+        K_ttm1.append(calc_diff_LLbar(op, ops_bar, obs, obs_t, op_system, fun_args=fun_args))
+
+    logger.debug('Concatenating K_ttm1; shapes are [{}]'.format([k.shape for k in K_ttm1]))
+    K_ttm1 = np.concatenate(K_ttm1)
+    logger.debug('About to call schur; shapes are: last_inv: {} K_ttm1: {}, K_t: {}; K_t_inv: {}'.format(
+        prev_inv.shape,
+        K_ttm1.shape,
+        K_t.shape,
+        K_t_inv.shape
+    ))
+    new_inv = schur(prev_inv, K_ttm1, K_t, K_t_inv)
+
+    posterior_t = collocation.CollocationPosterior(flatten_list(all_ops),
+                                                   flatten_list(all_ops_bar),
+                                                   op_system,
+                                                   flatten_list(all_obs),
+                                                   new_inv,
+                                                   fun_args=fun_args)
+
+    return posterior_t
+
+
+def flatten_list(list):
+    return [item for l in list for item in l]
 
 
 def calc_diff_LLbar(ops, ops_bar, obs_1, obs_2, op_system, fun_args):
