44aabc1e9f3f2e4883af4cf2564beb6eb1534b7d
Jon Cockayne <benorn@gmail.com>
2018-12-18 02:21:13 -0800

    Added dependencies

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     author='benorn',
     author_email='',
     description='',
-    requires=['numpy'],
+    requires=['cython', 'numpy', 'scipy', 'sympy'],
     ext_modules=cythonize('bayesian_pdes/pairwise.pyx'),
     include_dirs=[numpy.get_include()]
 )
